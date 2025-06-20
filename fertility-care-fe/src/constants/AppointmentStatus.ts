063362f7fcee769f1a99d357bb3484db2a8c6eea
Cao Thanh Duy <duyctse180149@fpt.edu.vn>
2025-06-21 00:57:36 +0700

    feat: add AppointmentStatus.ts

---
+++ fertility-care-fe/src/constants/AppointmentStatus.ts
@@ -0,0 +1,8 @@
+
+export const APPOINTMENT_BOOKED = "Booked";
+
+export const APPOINTMENT_CONFIRMED = "Confirmed";
+
+export const APPOINTMENT_COMPLETED = "Completed";
+
+export const APPOINTMENT_CANCELLED = "Cancelled";
