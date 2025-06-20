9403036a8086f34e455f364817c217df1b47baf0
Do Quoc Hung <hungdqse1800144@fpt.edu.vn>
2025-06-21 01:05:39 +0700

    feat: add PersonalInfo.ts

---
+++ fertility-care-fe/src/models/PersonalInfo.ts
@@ -0,0 +1,17 @@
+
+
+export default interface PersonalInfo {
+  firstName: string,
+  middleName: string,
+  lastName: string,
+  email: string,
+  address: string,
+  phone: string,
+  dateOfBirth: string,
+  gender: string,
+  medicalHistory: string,
+  partnerName: string,
+  partnerEmail: string,
+  partnerPhone: string,
+
+}
