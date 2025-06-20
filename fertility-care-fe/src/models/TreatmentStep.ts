87a82983870eff5dcb148a312ec329e029f658cd
Do Quoc Hung <hungdqse1800144@fpt.edu.vn>
2025-06-21 01:03:54 +0700

    feat: add TreatmentStep.ts

---
+++ fertility-care-fe/src/models/TreatmentStep.ts
@@ -0,0 +1,18 @@
+export interface TreatmentStep {
+
+    Id: string,
+
+    stepName: string,
+
+    description: string,
+
+    stepOrder: number,
+
+    estimatedDurationDays: number,
+
+    amount: number,
+
+    createdAt: string,
+
+    updatedAt: string
+}
