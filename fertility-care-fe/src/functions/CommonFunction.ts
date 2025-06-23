11e85720f0df00bdf805722b5c90c9aa156c195b
khaicoderlor <khainqse180154@fpt.edu.vn>
2025-06-23 10:26:24 +0700

    feat: implement ConvertFullName in common functions

---
+++ fertility-care-fe/src/functions/CommonFunction.ts
@@ -0,0 +1,13 @@
+import type Profile from "../models/Profile";
+import type { SlotSchedule } from "../models/SlotSchedule";
+import {
+  STEP_COMPLETED,
+  STEP_FAILED,
+  STEP_PROGRESS,
+} from "../constants/StepStatus";
+import type OrderStep from "../models/OrderStep";
+
+export function ConvertFullName(profile: Profile): string {
+  return profile.firstName + " " + profile.middleName + " " + profile.lastName;
+}
+
