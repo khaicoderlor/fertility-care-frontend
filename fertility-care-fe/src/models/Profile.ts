a6b994cd9060d83bd1bff6d866b4dc826715c3fa
khaicoderlor <khainqse180154@fpt.edu.vn>
2025-06-23 10:30:49 +0700

    feat: create profile model

---
+++ fertility-care-fe/src/models/Profile.ts
@@ -0,0 +1,13 @@
+export default interface Profile {
+  id?: string;
+  firstName?: string;
+  middleName?: string;
+  lastName?: string;
+  fullName?: string;
+  gender?: string;
+  dateOfBirth?: string;
+  address?: string;
+  avatarUrl?: string;
+  createdAt?: string;
+  updatedAt?: string;
+}
