14e5086b8b845460d4f7b74be3d46fb1ad2b0ed3
khaicoderlor <khainqse180154@fpt.edu.vn>
2025-06-23 10:57:48 +0700

    feat: implement Auth Provider and Route page

--- fertility-care-fe/src/App.tsx
+++ fertility-care-fe/src/App.tsx
@@ -1,10 +1,40 @@
+import { Route, Routes } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import BookingPage from "./pages/order/BookingPage";
+import { AuthProvider } from "./contexts/AuthContext";
+import LoginPage from "./pages/auth/LoginPage";
+import PrivateRoute from "./routes/PrivateRoute";
+import PatientProgressPage from "./pages/patient/PatientProgressPage";
+import DoctorDashboard from "./pages/doctor/DoctorDashboard";
+import FollowUpPatientProgressPage from "./pages/doctor/FollowUpPatientProgressPage";
+
 
 function App() {
 
 
   return (
     <>
-      <h1 className="text-red-500">Hello</h1>
+      <AuthProvider>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route
+            path="/order"
+            element={
+              <PrivateRoute>
+                <BookingPage />
+              </PrivateRoute>
+            }
+          />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/progress" element={
+            <PrivateRoute>
+              <PatientProgressPage/>
+            </PrivateRoute>
+          }/>
+          <Route path="/dashboard/doctor" element={<DoctorDashboard/>}/>
+          <Route path="/follow-up/patient/progress" element={<FollowUpPatientProgressPage />} />
+        </Routes>
+      </AuthProvider>
     </>
   )
 }
